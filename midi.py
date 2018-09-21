c4a888ced223f09775214d7a47e5913729d8907e
Eric Nichols <ernichol@microsoft.com>
2018-09-21 16:04:41 +0200

    Adding chunking-into-4-measures functionality.

--- midi.py
+++ midi.py
@@ -5,7 +5,7 @@ from itertools import chain
 
 
 PATH = 'Bach-Two_Part_Inventions_MIDI_Transposed'
-
+CHUNK_SIZE = 4  # MEASURES
 
 def get_notes(file):
     '''
@@ -30,29 +30,48 @@ def get_notes(file):
             notes.append('.'.join(str(n) for n in element.normalOrder))
     return '' .join(chain.from_iterable(notes))
 
-def get_parts(file):
+
+def convert_parts(file):
     piece = converter.parse(file)
+
+    # Find the max measure numbers.
+    measure_lens = []
     for part in piece.parts:
+        measures = part.makeMeasures()
+        measure_lens.append(len(measures))
+
+    max_measure = max(measure_lens) // CHUNK_SIZE * CHUNK_SIZE  # drop extra measures at the end
+
+    chunks = [(i, i + CHUNK_SIZE) for i in range(0, max_measure, 4)]
+
+    for i, part in enumerate(piece.parts):
         part_tuples=[]
-        try:
-            track_name = part[0].bestName()
-        except AttributeError:
-            track_name = 'None'
+        # try:
+        #     track_name = part[0].bestName()
+        # except AttributeError:
+        #     track_name = 'None'
+        track_name = file + 'part_%d' % i
         #part_tuples.append(track_name)
-        for event in part:
-            for y in event.contextSites():
-                if y[0] is part:
-                    offset=y[1]
-            if getattr(event,'isNote',None) and event.isNote:
-                string = " "+str(event.pitch.midi)+" "+str(event.quarterLength)
-                part_tuples.append(string)
-            if getattr(event,'isRest',None) and event.isRest:
-                string = " "+'Rest' + " "+ str(event.quarterLength)
-                part_tuples.append(string)
-        part_tuples = '' .join(chain.from_iterable(part_tuples))
-        with open(track_name+".txt", "w") as f:
-            f.write(str(part_tuples))
-            f.close()
+
+
+        # Make measures and go through measures
+        measures = part.makeMeasures()
+        for start, end in chunks:
+            part_tuples = []
+            track_name = file + 'part_%d' % i
+            for measure in measures[start: end]:
+                for event in measure:
+                    if getattr(event,'isNote',None) and event.isNote:
+                        string = " " + str(event.pitch.midi) + " " + str(event.quarterLength)
+                        part_tuples.append(string)
+                    if getattr(event,'isRest',None) and event.isRest:
+                        string = " " + 'R' + " " + str(event.quarterLength)
+                        part_tuples.append(string)
+            track_name += '_%d-%d' % (start, end)
+            part_tuples = '' .join(chain.from_iterable(part_tuples))
+            with open(track_name + ".txt", "w") as f:
+                f.write(str(part_tuples))
+                f.close()
 
 
 def convert_midi_to_txt(directory):
@@ -62,13 +81,20 @@ def convert_midi_to_txt(directory):
             f.write(string)
             f.close()
 
-def get_piece_chunks(filename):
-    print(file)
+def convert_midi_to_txt_chunks(directory):
+    for file in glob.glob(os.path.join(PATH, '*.mid')):
+        string = convert_parts(file)
+
+
+
+# def get_piece_chunks(filename):
+#     print(file)
 
 #if not os.path.exists(directory+"txt"):
 #    os.makedirs("txt")
 
 
 if __name__ == '__main__':
-    for file in glob.glob(os.path.join(PATH, '*.mid')):
-        get_piece_chunks(file)
+    convert_midi_to_txt_chunks(PATH)
+    # for file in glob.glob(os.path.join(PATH, '*.mid')):
+    #     get_piece_chunks(file)
