5737e94c8442c531de9fb04449f9b9ba1669eee7
Gianluca Micchi <gianluca.micchi@gmail.com>
2018-09-22 12:21:34 +0200

    training function for seq2seq. minor changes

--- src/tokenize_data.py
+++ src/tokenize_data.py
@@ -8,20 +8,20 @@ os.environ["KERAS_BACKEND"] = "tensorflow"
 
 data_path = '../Bach-Two_Part_Inventions_MIDI_Transposed/txt_tokenized'
 
-def tokenize_data():
+
+def tokenize_data(data_folder):
     # Vectorize the data. Returns a tuple of inputs, outputs.
     input_texts = []
     target_texts = []
     input_characters = set()
     target_characters = set()
 
-
-    for file in glob.glob("./*.txt"):
+    for file in sorted(glob.glob(os.path.join(data_folder, "*.txt"))):
         if file.split("_")[3] == "0":
             with open(file, "r") as f:
                 content = f.read().strip().split(' ')
                 for char in content:
-                    #print(char)
+                    # print(char)
                     if not char == '':
                         input_texts.append(char)
                         if char not in input_characters:
@@ -37,7 +37,6 @@ def tokenize_data():
 
     input_texts = np.array([int(e) for e in input_texts])
     target_texts = np.array([int(e) for e in target_texts])
-
 
     input_cat = to_categorical(input_texts, num_classes=128)
     target_cat = to_categorical(target_texts, num_classes=128)
